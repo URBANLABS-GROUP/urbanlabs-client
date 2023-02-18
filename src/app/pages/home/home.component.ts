ef4d448d53cce3470a78396c984cd56be8468e10
Ivan Chagin <chagin.i@firstmk.ru>
2023-02-18 15:59:47 +0300

    feat: Добавлен баннер, напоминающий об оплате аренды

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
 
 @Component({
   selector: 'urb-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  public closeBanner() {
+    const banner = document.getElementById("banner")
+    if (banner !== null) {
+      banner.style.display = "none"
+      document.querySelectorAll(".blur").forEach(
+          el => {
+            (el as HTMLElement).style.filter = "none";
+            (el as HTMLElement).style.pointerEvents = "auto"
+          });
+    }
+    localStorage.setItem("banner", "banner-off")
+  }
 
+  public ngOnInit() {
+    if (localStorage.getItem("banner") === "banner-off") {
+      this.closeBanner()
+    }
+  }
 }
